3529e321ae25672fa5a0ae4f5bceab7f45a46a08
BobTheFirst7777 <71926943+BobTheFirst7777@users.noreply.github.com>
2021-08-14 19:08:07 +0700

    Update TweepyPrototype.py

--- TweepyPrototype.py
+++ TweepyPrototype.py
@@ -7,11 +7,21 @@ auth.set_access_token("871671216901427201-8xLxXu1mGnw9hW268mlTHSn0ayvbXD7",
                       "soFJg6EQbCJmJdj014Zbdeo2MxJlhylBTLyF3ejclZZ3m")
 api = tweepy.API(auth)
 
+trendsList = list()
+
+output = list()
 
 
 
-trendsDICK = api.trends_place(2295414)
-for trend in trendsDICK[0]["trends"]:
-    print(trend["name"])
+trendsDick = api.trends_place(2295414)
+for trend in trendsDick[0]["trends"]:
+    trendsList.append(trend["name"])
 
+
+for i in range(0,4):
+    output.append(trendsList[i])
+    for tweet in api.search(q=trendsList[i],lang="en",count = 30,result_type="popular"):
+        output.append("https://twitter.com/twitter/statuses/"+str(tweet.id))
+
+print(output)
 
